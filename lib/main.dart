189d002fff6fd7d88f9acbf7441ba2bdc80e5893
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2024-01-01 14:38:57 +0700

    initilize Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/firebase_options.dart';
 import 'package:flutter_instagram_clone/presentation/pages/main_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
